5036c8e788ea31d1411f55f5fda4e7567a0a45b9
Eduardo Alves Di Nizo <eduardodinizo@hotmail.com>
2016-04-24 01:10:56 -0300

    Adicionado front end ao projeto
    Testar front end pela url http://localhost:8080/e-gammer/Test!blah

--- e-gammer/src/br/com/fatec/egammer/web/action/TestAction.java
+++ e-gammer/src/br/com/fatec/egammer/web/action/TestAction.java
@@ -5,17 +5,17 @@ import com.opensymphony.xwork2.ActionSupport;
 public class TestAction extends ActionSupport {
 
 //	private PapelDTO papel;
+	
 //
-//	public static final String JSP = "jsp";
+	public static final String JSP = "html";
 //
 //	/** */
-//	private static final long serialVersionUID = 1071989853380980252L;
+	private static final long serialVersionUID = 1071989853380980252L;
 //
-//	public String blah() {
-//		this.papel = new PapelDTO(1l, "papel", "descricao papel",
-//				new GrupoDTO(1l, "grupo", "desc grupo"));
-//		return JSP;
-//	}
+	public String blah() {
+		
+		return JSP;
+	}
 //
 //	public PapelDTO getPapel() {
 //		return this.papel;
